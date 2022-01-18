bb9e159375d9c9228d441dc82c2945bcd115aa37
Chris Ochs <chris@ochsnet.com>
2022-01-17 18:01:41 -0800

    doc and pool updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tachyon
 
-Tachyon is a reliable udp library that uses a nack based model. It provides ordered and unordered channels as well as fragmentation.
+Tachyon is a performant and highly parallel reliable udp library that uses a nack based model. It provides ordered and unordered channels as well as fragmentation.
 
 ## Reliablity
 Reliabilty is based on message receive windows of a maximum size, the default being 512.  The window is defined as the last in order message through the last received message.  If the difference exceeds the max the start of the window is increased by one, and the missing packet for that slot is then lost.
@@ -16,9 +16,7 @@ But you do need to be mindful of how this works, so that for every channel you h
 
 ## Channels
 Sequencing is per channel. With every connected address (connection) having it's own set of channels.  And per channel stats that are recorded.
-The system configures two channels automatically channel 1 being ordered and channel 2 unordered. And you can add more but they need to be added before bind/connect.  Because they are per address, on the server side we lazily create channels as we see receives from new addresses.  This means there are a lot of channels, so creating channels unnecessarily is a bad idea.  You don't need a lot of channels, at most 1-2 extra ordered channels if you have classes of messages that vary a lot in size AND are actually competing, ie possibly being sent at the same time.
-
-
+The system configures two channels automatically channel 1 being ordered and channel 2 unordered. And you can add more but they need to be added before bind/connect.  Because they are per address, on the server side we lazily create channels as we see receives from new addresses. 
 
 ## Fragmentation
 Fragments are individually reliable.  Each one is sent as a separate sequenced message.  So larger messages work fairly well, just not too large where you start chewing up too much of the receive window.
@@ -32,15 +30,15 @@ Unreliable messages have a hot path where there is almost no processing done.  R
 ## Connection management
 This is best managed outside the library itself mostly, but I will be adding an approved address map so receives can early out.  That's about as much as I think a library at this level should be doing.
 
-## Performance and concurrency
-Performance is fairly good but server side depends a lot on network packet loss.  Cpu time goes up as receive windows grow.  I think around 2k clients or so is likely around the max it will handle right now with some medium packet loss.  Past that it's likely to just start taking too long, but this is really going to vary a lot based on hardware and packet loss patterns.
+## Concurrency
+The best concurrency is no concurrency. Tachyon is parallel but uses no concurrency primitives internally. Instead it leverages the nature of Udp by simply running multiple Tachyon instances one per port.  And provides a Pool abstraction to manage these instances and run their receive flows in parallel. 
 
-Which brings me to concurrency.  The patterns that can take this to where it can bury the hardware are fairly straight forward, but the Rust compiler can't reason about those. So that's my main challenge atm plus I'm fairly new to Rust.  The core design I want is batch based.  Receive from the network into a set of batches, then process those partitioned over a thread per batch.  Fairly simple flow with minimal number of atomic ops (but an additional memcpy). But to Rust its' all shared state.
+Concurrent sending is supported for unreliable but not reliable. For reliable sends you have to send from one thread at a time.  Udp sockets are atomic at the OS level, so unreliable is just a direct path to that. UnreliableSender is a struct that can be created for use in other threads. That is a rust thing it's not needed for actual thread safety it's just needed for Rust to know it's safe.
 
-In the end I might just end up solving this using multiple ports. Add a batching send/receive api and that might be the simplest route here. 
+Parallel receiving does have additional overhead.  It allocates bytes for received messages and then finally pushes those all to a single consumer queue. The design is a concurrent queue of non concurrent queues, so a parallel receive we do a single atomic pop to get the regular queue, and another one to push it back on the concurrent queue once receiving is done. Other then the countdown event for waiting on the parallel work that's the entirety of the concurrency.
 
-Sends are interesting because udp send is atomic at the OS level. It's perfectly safe to just send from any thread.  And the Tachyon unreliable path is thread safe, although again the Rust compiler can't reason about that.  But via ffi Rust doesn't know it's being used from different threads.
+##Performance
+Cpu time is mostly in udp syscalls. But very heavy packet loss can also increase cpu time because larger receive windows make Tachyon itself do more work as it has to iterate over those.
 
 ## Usage
 Right now unfortunately not any documentation.  The best guide is look at the tests in tachyon.rs and at ffi.rs.  tachyon_tests.rs has some stress testing unit tests.  The api is designed primarily for ffi consumption, as I use it from a .NET server.
-
--- src/tachyon/pool.rs
+++ src/tachyon/pool.rs
@@ -1,9 +1,10 @@
 
-use std::{sync::{Arc, Mutex}, time::Instant, collections::VecDeque};
+use std::{sync::{Arc, Mutex}, time::Instant, collections::VecDeque, panic};
 
 use crossbeam::queue::ArrayQueue;
 use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
 use rustc_hash::FxHashMap;
+use synchronoise::CountdownEvent;
 
 use super::{Tachyon, TachyonConfig, network_address::NetworkAddress};
 
@@ -15,6 +16,8 @@ pub struct Pool {
     pub receive_queue: Arc<ArrayQueue<VecDeque<Vec<u8>>>>,
     pub receive_buffers: Arc<ArrayQueue<Vec<u8>>>,
     pub published: VecDeque<Vec<u8>>,
+    pub servers_in_use: Arc<ArrayQueue<Tachyon>>,
+    pub counter: Option<Arc<CountdownEvent>>
 }
 
 impl Pool {
@@ -25,13 +28,16 @@ impl Pool {
             queue.push(VecDeque::new());
             receive_buffers.push(vec![0;1024 * 1024]);
         }
-      
+        let in_use:ArrayQueue<Tachyon> = ArrayQueue::new(MAX_SERVERS);
+
         let pool = Pool {
             next_id: 0,
             servers: FxHashMap::default(),
             receive_queue: Arc::new(queue),
             receive_buffers: Arc::new(receive_buffers),
-            published: VecDeque::new()
+            published: VecDeque::new(),
+            servers_in_use: Arc::new(in_use),
+            counter: None
         };
         return pool;
     }
@@ -61,14 +67,18 @@ impl Pool {
         return self.published.pop_front();
     }
 
-    pub fn move_received_to_published(&mut self) {
-        let receive_queue_clone = self.receive_queue.clone();
-        if let Some(mut receive_queue) = receive_queue_clone.pop() {
-            for value in receive_queue.drain(..) {
-                self.published.push_back(value);
+    pub fn move_received_to_published(&mut self) -> i32 {
+        let mut count = 0;
+        for _ in 0..self.receive_queue.len() {
+            if let Some(mut receive_queue) = self.receive_queue.pop() {
+                for value in receive_queue.drain(..) {
+                    self.published.push_back(value);
+                    count += 1;
+                }
+                self.receive_queue.push(receive_queue).unwrap_or_default();
             }
-            receive_queue_clone.push(receive_queue).unwrap_or_default();
         }
+        return count;
     }
 
     fn receive_server(server: &mut Tachyon, receive_queue: &mut VecDeque<Vec<u8>>, receive_buffer: &mut Vec<u8>) {
@@ -90,7 +100,7 @@ impl Pool {
         }
     }
 
-    pub fn receive(&mut self) {
+    pub fn receive_blocking(&mut self) {
         self.servers.par_iter_mut().for_each(|(key,server)|{
             let receive_queue_clone = self.receive_queue.clone();
             let receive_buffers_clone = self.receive_buffers.clone();
@@ -104,6 +114,72 @@ impl Pool {
             }
         });
         self.move_received_to_published();
+    }
+
+    pub fn finish_receive(&mut self) -> (u32, i32) {
+        let mut server_count = 0;
+        let mut message_count = 0;
+
+        match &self.counter {
+            Some(counter) => {
+                counter.wait();
+                message_count += self.move_received_to_published();
+
+                for _ in 0..self.servers_in_use.len() {
+                    if let Some(server) = self.servers_in_use.pop() {
+                        self.servers.insert(server.id, server);
+                        server_count += 1;
+                    }
+                }
+                self.counter = None;
+            },
+            None => {},
+        }
+        return (server_count, message_count);
+    }
+    
+    pub fn receive(&mut self) -> bool {
+
+        let server_count = self.servers.len();
+        if server_count == 0 {
+            return false;
+        }
+
+        let counter = Arc::new(CountdownEvent::new(server_count));
+        
+        let in_use = self.servers_in_use.clone();
+        for s in self.servers.drain() {
+            let server = s.1;
+            in_use.push(server);
+        }
+
+        for _ in 0..server_count {
+            let in_use = self.servers_in_use.clone();
+            let receive_queue_clone = self.receive_queue.clone();
+            let receive_buffers_clone = self.receive_buffers.clone();
+            let signal = counter.clone();
+
+            rayon::spawn(move || {
+                match in_use.pop() {
+                    Some(mut server) => {
+                        if let Some(mut receive_queue) = receive_queue_clone.pop() {
+                            if let Some(mut receive_buffer) = receive_buffers_clone.pop() {
+                                Pool::receive_server(&mut server, &mut receive_queue, &mut receive_buffer);
+                                receive_buffers_clone.push(receive_buffer).unwrap_or_default();
+                            }
+                            receive_queue_clone.push(receive_queue).unwrap_or_default();
+                        }
+                        in_use.push(server);
+                    },
+                    None => {
+    
+                    },
+                }
+                signal.decrement().unwrap();
+             });
+        }
+        self.counter = Some(counter);
+        return true;
     }
 
 }
@@ -120,7 +196,7 @@ mod tests {
     use super::Pool;
     
     #[test]
-    fn test_pool_run() {
+    fn test_receive() {
         let mut pool = Pool::create();
         let config = TachyonConfig::default();
         pool.create_server(config, NetworkAddress::localhost(8001));
@@ -144,7 +220,54 @@ mod tests {
         }
 
         let now = Instant::now();
-        pool.receive();
+        let receiving = pool.receive();
+        assert!(receiving);
+
+        // should return false, all servers moved
+        let receiving = pool.receive();
+        assert!(!receiving);
+
+        let res = pool.finish_receive();
+        assert_eq!(3, res.0);
+        assert_eq!(count * 3, res.1);
+        assert_eq!(count * 3, pool.published.len() as i32);
+
+        // nothing to finish
+        let res = pool.finish_receive();
+        assert_eq!(0, res.0);
+        assert_eq!(0, res.1);
+
+        let elapsed = now.elapsed();
+        println!("Elapsed: {:.2?}", elapsed);
+        
+    }
+
+    #[test]
+    fn test_blocking_receive() {
+        let mut pool = Pool::create();
+        let config = TachyonConfig::default();
+        pool.create_server(config, NetworkAddress::localhost(8001));
+        pool.create_server(config, NetworkAddress::localhost(8002));
+        pool.create_server(config, NetworkAddress::localhost(8003));
+
+        let mut client1 = TachyonTestClient::create(NetworkAddress::localhost(8001));
+        let mut client2 = TachyonTestClient::create(NetworkAddress::localhost(8002));
+        let mut client3 = TachyonTestClient::create(NetworkAddress::localhost(8003));
+        client1.connect();
+        client2.connect();
+        client3.connect();
+
+        let count = 20000;
+        let msg_len = 64;
+
+        for _ in 0..count {
+            client1.client_send_reliable(1, msg_len);
+            client2.client_send_reliable(1, msg_len);
+            client3.client_send_reliable(1, msg_len);
+        }
+
+        let now = Instant::now();
+        pool.receive_blocking();
 
         let elapsed = now.elapsed();
         println!("Elapsed: {:.2?}", elapsed);
